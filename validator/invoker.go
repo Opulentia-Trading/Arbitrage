040033d7e0a4ce1516eacd2ea6ccc9a116cdc0ab
Elikem Hermon <eliherm@gmail.com>
2022-02-24 16:22:40 -0500

    Moves validator code into the validator package

--- validator/invoker.go
+++ validator/invoker.go
@@ -1,11 +1,11 @@
-package main
+package validator
 
-// The Invoker class is responsible for initiating requests. 
-// This class stores a reference to a queue of command objects. 
-// Invoker triggers that command instead of sending the request directly to the receiver. 
+// The Invoker class is responsible for initiating requests.
+// This class stores a reference to a queue of command objects.
+// Invoker triggers that command instead of sending the request directly to the receiver.
 // Invoker doesn't create the command object, it gets a pre-created command from the client via the constructor.
 type Invoker struct {
-    commands []Command
+	commands []Command
 }
 
 func NewInvoker() *Invoker {
@@ -14,7 +14,7 @@ func NewInvoker() *Invoker {
 
 // Adds a new command to current queue of commands
 func (i *Invoker) Add(command Command) {
-    i.commands = append(i.commands, command)
+	i.commands = append(i.commands, command)
 }
 
 // The ExecuteCommands method executes all the commands one by one
@@ -22,5 +22,5 @@ func (i *Invoker) ExecuteCommands() {
 	for _, command := range i.commands {
 		command.execute()
 	}
-    i.commands = []Command{}
+	i.commands = []Command{}
 }
